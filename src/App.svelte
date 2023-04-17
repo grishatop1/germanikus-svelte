2255608823fd3a1e2d7740a2dc8634270a2f8f32
Stefan Šućur <stefansucur0@gmail.com>
2023-04-18 01:57:28 +0200

    Move checkbox to separate file

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import Input from "./lib/Input.svelte";
   import Info from "./lib/Info.svelte";
   import Keyboard from "./lib/Keyboard.svelte";
+  import Checkbox from "./lib/Checkbox.svelte";
   import { userInputStore, decodeStore } from "./stores";
   import { toGermanikus, fromGermanikus } from "./consts/germanikus";
   import { blur } from "svelte/transition";
@@ -14,11 +15,12 @@
     userInput = value;
   })
 
-  $: userInput, convert()
-
   let decodeInput = false;
-  $: decodeStore.set(decodeInput)
+  decodeStore.subscribe(value => {
+    decodeInput = value;
+  })
 
+  $: userInput, convert()
   $: decodeInput, convert()
 
   let output = [];
@@ -40,19 +42,7 @@
 <h1 style="margin: 10px;">Germanikus Cipher</h1>
 
 <Input />
-
-<label>
-  <div class="switch-with-text">
-    Encode
-    <div class="switch">
-      <div class="container">
-        <input class="checkbox" type="checkbox" bind:checked={decodeInput}>
-        <div class="slider round"></div>
-      </div>
-    </div>
-    Decode
-  </div>
-</label>
+<Checkbox />
 
 <div class="output-letter">
   {#each output as letter}
@@ -90,52 +80,5 @@
     left: 30px;
     font-size: 1.5em;
     user-select: none;
-  }
-
-  .switch-with-text {
-    display:flex;
-    flex-direction:row;
-    justify-content:space-evenly;
-  }
-  .switch {
-    display: inline-block;
-    height: 34px;
-    position: relative;
-    width: 60px;
-  }
-  .switch input {
-    display:none;
-  }
-  .slider {
-    background-color: #fffd;
-    bottom: 0;
-    cursor: pointer;
-    left: 0;
-    position: absolute;
-    right: 0;
-    top: 0;
-    transition: .4s;
-  }
-  .slider:before {
-    background-color: #24242a;
-    bottom: 4px;
-    content: "";
-    height: 26px;
-    left: 4px;
-    position: absolute;
-    transition: .1s;
-    width: 26px;
-  }
-  input:checked + .slider {
-    background-color: #fffd;
-  }
-  input:checked + .slider:before {
-    transform: translateX(26px);
-  }
-  .slider.round {
-    border-radius: 34px;
-  }
-  .slider.round:before {
-    border-radius: 50%;
   }
 </style>
