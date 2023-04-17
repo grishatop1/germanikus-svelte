cc9348d64cc462c4edb7deb580e1a648b8e785d5
Stefan Šućur <stefansucur0@gmail.com>
2023-04-17 23:49:28 +0200

    Move Input out of label, add sliding checkbox
    Needs additional CSS tweaking to push text closer to checkbox

--- src/App.svelte
+++ src/App.svelte
@@ -39,10 +39,19 @@
 <div class="content">
 <h1 style="margin: 10px;">Germanikus Cipher</h1>
 
+<Input />
+
 <label>
-  <Input />
-  Decode
-  <input type="checkbox" bind:checked={decodeInput}>
+  <div class="switch-with-text">
+    Encode
+    <div class="switch">
+      <div class="container">
+        <input class="checkbox" type="checkbox" bind:checked={decodeInput}>
+        <div class="slider round"></div>
+      </div>
+    </div>
+    Decode
+  </div>
 </label>
 
 <div class="output-letter">
@@ -81,5 +90,52 @@
     left: 30px;
     font-size: 1.5em;
     user-select: none;
+  }
+
+  .switch-with-text {
+    display:flex;
+    flex-direction:row;
+    justify-content:space-evenly;
+  }
+  .switch {
+    display: inline-block;
+    height: 34px;
+    position: relative;
+    width: 60px;
+  }
+  .switch input {
+    display:none;
+  }
+  .slider {
+    background-color: #fffd;
+    bottom: 0;
+    cursor: pointer;
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    transition: .4s;
+  }
+  .slider:before {
+    background-color: #24242a;
+    bottom: 4px;
+    content: "";
+    height: 26px;
+    left: 4px;
+    position: absolute;
+    transition: .1s;
+    width: 26px;
+  }
+  input:checked + .slider {
+    background-color: #fffd;
+  }
+  input:checked + .slider:before {
+    transform: translateX(26px);
+  }
+  .slider.round {
+    border-radius: 34px;
+  }
+  .slider.round:before {
+    border-radius: 50%;
   }
 </style>
